848f733f92a3147022215abc77dada408d5ec1b1
Cássio Rodrigues <curriculocassio@hotmail.com>
2022-11-27 19:29:26 -0300

    add repository crud

---
+++ Cfr.Pizzas.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Cfr.Pizzas.Domain.Models;
+using System.Linq.Expressions;
+
+namespace Cfr.Pizzas.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Adicionar(TEntity entity);
+        Task<TEntity> ObterPorId(Guid id);
+        Task<List<TEntity>> ObterTodos();
+        Task Atualizar(TEntity entity);
+        Task Remover(Guid id);
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+        Task<int> SaveChanges();
+    }
+}
